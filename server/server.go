2bedfcd7f31f565397956799c7fee7ca94583367
Arvin619 <wasd572@gmail.com>
2023-06-07 21:21:53 +0800

    add log file

--- server/server.go
+++ server/server.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"strconv"
 	"syscall"
+	"time"
 
 	pb "github.com/Arvin619/grpc-demo/proto"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"google.golang.org/grpc"
 )
 
@@ -40,7 +44,7 @@ func (gs *GreeterServer) SayList(pr *pb.HelloRequest, ss pb.Greeter_SayListServe
 
 func (gs *GreeterServer) SayRecord(srs pb.Greeter_SayRecordServer) error {
 	for {
-		req, err := srs.Recv()
+		_, err := srs.Recv()
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				srs.SendAndClose(&pb.HelloReply{Message: "Done~"})
@@ -49,7 +53,7 @@ func (gs *GreeterServer) SayRecord(srs pb.Greeter_SayRecordServer) error {
 				panic(err)
 			}
 		}
-		log.Println(req.Name)
+		// log.Println(req.Name)
 	}
 }
 
@@ -57,7 +61,7 @@ func (gs *GreeterServer) SayRoute(srs pb.Greeter_SayRouteServer) error {
 	n := 0
 	for {
 		_ = srs.Send(&pb.HelloReply{Message: "say.route" + strconv.Itoa(n)})
-		req, err := srs.Recv()
+		_, err := srs.Recv()
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				return nil
@@ -66,12 +70,63 @@ func (gs *GreeterServer) SayRoute(srs pb.Greeter_SayRouteServer) error {
 			}
 		}
 		n++
-		log.Println(req.Name)
+		// log.Println(req.Name)
 	}
 }
 
+func createLogFile() (logFile *os.File, closeFun func()) {
+	var err error
+	if _, err = os.Stat("./log"); errors.Is(err, os.ErrNotExist) {
+		if err = os.Mkdir("./log", os.ModePerm); err != nil {
+			panic(err)
+		}
+	}
+	logFileName := time.Now().Format(time.ANSIC) + ".log"
+	logFile, err = os.Create(filepath.Join("./log", logFileName))
+	if err != nil {
+		panic(err)
+	}
+	closeFun = func() {
+		logFile.Close()
+	}
+	return
+}
+
+func InterceptorLogger(l *log.Logger) logging.Logger {
+	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
+		switch lvl {
+		case logging.LevelDebug:
+			msg = fmt.Sprintf("DEBUG :%v", msg)
+		case logging.LevelInfo:
+			msg = fmt.Sprintf("INFO :%v", msg)
+		case logging.LevelWarn:
+			msg = fmt.Sprintf("WARN :%v", msg)
+		case logging.LevelError:
+			msg = fmt.Sprintf("ERROR :%v", msg)
+		default:
+			panic(fmt.Sprintf("unknown level %v", lvl))
+		}
+		l.Println(append([]any{"msg", msg}, fields...))
+	})
+}
+
 func main() {
-	server := grpc.NewServer()
+	file, close := createLogFile()
+	defer close()
+
+	logger := InterceptorLogger(log.New(io.MultiWriter(os.Stdout, file), "", log.Ldate|log.Ltime|log.Lshortfile))
+	logOpt := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+	}
+
+	server := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			logging.UnaryServerInterceptor(logger, logOpt...),
+		),
+		grpc.ChainStreamInterceptor(
+			logging.StreamServerInterceptor(logger, logOpt...),
+		),
+	)
 	pb.RegisterGreeterServer(server, &GreeterServer{})
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
